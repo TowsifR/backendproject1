702223e6a6a05564d730a84c8c773d3c0dcc296a
TowsifR <milantowsif77@gmail.com>
2025-03-15 15:26:51 -0400

    Install prometheus_fastapi_instrumentator

--- app/backend/api/main.py
+++ app/backend/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/health")
+async def health():
+    return {"Status": "Healthy!"}
